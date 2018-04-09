a885961f9a940b48cc5aca2ab4f72a8ac90df0b5
JBStokley@Outlook.com <JBStokley@Outlook.com>
2018-04-09 17:41:34 -0400

    Initializes world abstract class.

--- AdventureEngine/AdventureEngine/Game/Worlds/World.cs
+++ AdventureEngine/AdventureEngine/Game/Worlds/World.cs
@@ -1,11 +1,58 @@
 ﻿using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 
-public interface World
+public abstract class World
 {
-    bool Visible { get; set; }
-    List<Entity> Entities { get; }
+    public bool Visible { get; set; }
+    public List<Entity> Entities { get; }
 
-    void Update(GameTime gameTime);
-    void Draw(GameTime gameTime);
+
+    protected bool _visible;
+    protected List<Entity> _entities;
+
+
+    public World(bool visible = false)
+    {
+        _visible = visible;
+        _entities = new List<Entity>();
+    }
+
+
+    public virtual void Update(GameTime gameTime)
+    {
+        foreach(Entity ent in _entities)
+            ent.Update(gameTime);
+    }
+
+    public virtual void Draw(GameTime gameTime)
+    {
+        foreach(Entity ent in _entities)
+            ent.Draw(gameTime);
+    }
+
+    /// <summary>
+    /// Attaches an entity to a world.
+    /// </summary>
+    /// <param name="entity">The entity to be attached.</param>
+    public virtual void Add(Entity entity)
+    {
+        if (!_entities.Contains(entity))
+        {
+            entity.ParentWorld = this;
+            _entities.Add(entity);
+        }
+    }
+
+    /// <summary>
+    /// Removes an entity from a world.
+    /// </summary>
+    /// <param name="entity">The entity to be removed.</param>
+    public virtual void Remove(Entity entity)
+    {
+        if (_entities.Contains(entity))
+        {
+            entity.ParentWorld = null;
+            _entities.Remove(entity);
+        }
+    }
 }
